80ad9a2599d015eb9dfe06b4151e9618931b2a4a
RizaAthaya <athayarizaaa@gmail.com>
2024-03-21 18:31:44 +0700

    fix(deploy): bug image deploy

--- src/component/home/section/HeroSection.jsx
+++ src/component/home/section/HeroSection.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from "react";
-import Button from "../../ui/Button";
 import { useNavigate } from "react-router-dom";
+import Computer from "../../../assets/home/Computer.png";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { ArrowDowns } from "../../ui/Icons";
 
 const HeroSection = () => {
   const navigate = useNavigate();
-  
+
   useEffect(() => {
     AOS.init({
       offset: 50,
@@ -17,7 +18,10 @@ const HeroSection = () => {
   }, []);
 
   return (
-    <section data-aos="fade-up" className="px-8 lg:px-[100px] min-h-screen flex items-center justify-center w-full bg-[url(../src/assets/home/Hero-mobile.png)] lg:bg-contain bg-cover lg:bg-[url(../src/assets/home/Hero.png)] bg-no-repeat bg-right-top">
+    <section
+      data-aos="fade-up"
+      className=" relative px-8 lg:px-[100px] min-h-screen flex flex-col items-center justify-center w-full bg-[url(../src/assets/home/Hero-mobile.png)] lg:bg-contain bg-cover lg:bg-[url(../src/assets/home/Hero.png)] bg-no-repeat bg-right-top"
+    >
       <div className="flex lg:flex-row flex-col lg:gap-0 gap-8 items-center justify-between max-w-[1440px] h-full">
         <div className="w-full lg:w-[calc(50%-50px)] flex flex-col gap-8">
           <div className="flex flex-col gap-4">
@@ -28,17 +32,13 @@ const HeroSection = () => {
               Get to know more about Country, Regions, sub-Regions, flags,
               capital city and many more
             </p>
+            <div className="absolute bottom-8 lg:right-1/2 right-8 animate-bounce flex p-2 bg-blue-secondary rounded-full justify-center items-center w-fit text-white mt-3">
+              <ArrowDowns />
+            </div>
           </div>
-          <Button
-            variation={"primary"}
-            className={"w-fit"}
-            onClick={() => navigate("#countries")}
-          >
-            Get Started
-          </Button>
         </div>
         <img
-          src="../src/assets/home/Computer.png"
+          src={Computer}
           className="xl:w-auto lg:max-w-none max-w-[calc(100%-60px)] md:max-w-[calc(100%-100px)] lg:w-[400px] lg:mx-0 mx-none"
         />
       </div>
